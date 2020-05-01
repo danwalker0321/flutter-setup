ed46887cdc91731ef3566be1b2e2ebd96ebc36fd
danwalker0321 <dan.walker0321@gmail.com>
2020-05-02 03:27:23 +0800

    Initial Commit

---
+++ android/app/src/main/kotlin/danwalker0321/fluttersetup/MainActivity.kt
@@ -0,0 +1,12 @@
+package danwalker0321.fluttersetup
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
